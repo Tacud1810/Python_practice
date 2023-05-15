c6d31ddcdad9c61724299a5e9085413d1cc4fa9e
Tacud1810 <r.kachyna@gmail.com>
2023-05-15 19:44:41 +0200

    lists_final.py - only first task

--- Practice/lists_final.py
+++ Practice/lists_final.py
@@ -1,4 +1,3 @@
-
 # 1. Write a function that takes a dictionary where keys are student names and values are lists of their grades,
 #    and returns a dictionary where keys are student names and values are their average grades.
 # 2. Write a function that takes a list of tuples where each tuple contains a movie title, release year,
@@ -18,11 +17,9 @@ def average_student_rating(students_ratings: dict):
         avg = sum(grades) / len(grades)
         avg_dict[name] = avg
     return avg_dict
-        # print(i_new)
-        # average_rating.update(i_new)
+    # print(i_new)
+    # average_rating.update(i_new)
     # return average_rating
-
-
 
 
 students = {
@@ -31,4 +28,72 @@ students = {
     'Charlie': [5, 4, 5, 5],
     'David': [3, 3, 3, 4]
 }
-print(average_student_rating(students))
+
+
+# print(average_student_rating(students))
+# ad 2:
+def highest_rated_movie(movies_list):
+    highest_rating = 0
+    highest_movie = ""
+
+    for movie in movies_list:
+        title, year, rating = movie
+        if rating > highest_rating:
+            highest_rating = rating
+            highest_movie = title
+
+    return highest_movie
+
+
+# ad 3:
+def reverse_dictionary(state_towns):
+    town_state = {}
+    for key, value in state_towns.items():
+        for i in value:
+            town_state.update({i: key})
+    return town_state
+
+
+# ad 4:
+def plant_food_animal(animalsfood):
+    plant_foods = ["carrot", "apple", "lettuce", "peas", "banana", "beetroot", "orange",
+                   "parsley", "strawberries", "broccoli", "Grass", "Leaves", "Fruits", "Twigs", "Eucalyptus leaves",
+                   "Bamboo", "Hay", "Leafy vegetables"]
+    # vege_animals = []
+    # for key, value in animalsfood.items():
+    #     for i in value:
+    #         if i in plant_foods:
+    #             if key not in vege_animals:
+    #                 vege_animals.append(key)
+    # return vege_animals
+
+    vege_animals = []
+    for key, value in animalsfood.items():
+        if any(food in plant_foods for food in value):
+            vege_animals.append(key)
+    return vege_animals
+
+
+animal_food_dict = {
+    "Elephant": ["Grass", "Leaves", "Fruits"],
+    "Giraffe": ["Leaves", "Twigs", "Fruits"],
+    "Koala": ["Eucalyptus leaves"],
+    "Panda": ["Bamboo", "Fruits"],
+    "Rabbit": ["Grass", "Hay", "Leafy vegetables"],
+    "Lion": ["Meat", "Zebra", "Buffalo"],
+    "Cheetah": ["Antelope", "Gazelle", "Rabbit"],
+    "Wolf": ["Deer", "Elk", "Moose"],
+    "Eagle": ["Fish", "Small mammals", "Birds"],
+    "Crocodile": ["Fish", "Antelope", "Water birds"],
+    "Tiger": ["Meat", "Fish", "Vegetables"],
+    "Bear": ["Fish", "Berries", "Honey"],
+    "Raccoon": ["Fruits", "Nuts", "Insects"],
+    "Fox": ["Small mammals", "Birds", "Grass"],
+    "Opossum": ["Fruits", "Insects", "Eggs"]
+}
+
+
+# ad 5:
+def sorted_list(list_of_tuples):
+    list_of_tuples.sort()
+    return list_of_tuples
