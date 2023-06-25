7ac6d56e57105f12e620888946d483484f23dafa
Tacud1810 <r.kachyna@gmail.com>
2023-06-25 10:33:52 +0200

    exercism practice - scrabble score

---
+++ Exercism/scrabble_score.py
@@ -0,0 +1,24 @@
+"""Instructions
+
+Given a word, compute the Scrabble score for that word."""
+
+VALUES_2 = {
+	1: "A, E, I, O, U, L, N, R, S, T",
+	2: "D, G",
+	3: "B, C, M, P ",
+	4: "F, H, V, W, Y",
+	5: "K",
+	8: "J, X",
+	10: "Q, Z"
+}
+
+
+def score(word):
+	"""
+	:param word:
+	:return: scrabble score
+	"""
+	result = []
+	for letter in word.upper():
+		result += [key for key, value in VALUES_2.items() if letter in value]
+	return sum(result)
